688e7704273611dc11f37ed5e825a6eb8ea5707b
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-12 11:15:21 -0800

    Will authenticate in interface, and print out user email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
     @user = User.authenticate(params[:email], params[:password])
     if @user
@@ -9,5 +12,11 @@ class SessionsController < ApplicationController
       flash[:alert] = "There was a problem logging you in"
       redirect_to log_in_path
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You've been logged out successfully"
+    redirect_to '/'
   end
 end
